ffb43e0afe0abc0b44a77a0271ee7460cb8b09e0
SagarMakhar <sagarmakhar@gmail.com>
2024-06-26 00:45:42 +0800

    fixup! base: BiometricScheduler: Cancel operation if not idle
    - some hals fail to report success/failure (for ex. realme fp hals)
    [DarkJoker360 - Switch to overlays]
    
    Change-Id: I442ce063280af36a04c25fcbc3dd45a90f196988
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- services/core/java/com/android/server/biometrics/sensors/BiometricScheduler.java
+++ services/core/java/com/android/server/biometrics/sensors/BiometricScheduler.java
@@ -73,6 +73,8 @@ public class BiometricScheduler<T, U> {
 
     private static final String TAG = "BiometricScheduler";
 
+    private boolean mCancel;
+
     // Number of recent operations to keep in our logs for dumpsys
     protected static final int LOG_NUM_RECENT_OPERATIONS = 50;
 
@@ -183,9 +185,6 @@ public class BiometricScheduler<T, U> {
         }
     }
 
-    private final boolean mCancelIfNotIdle = Resources.getSystem().getBoolean(
-            com.android.internal.R.bool.config_fpCancelIfNotIdle);;
-
     // Internal callback, notified when an operation is complete. Notifies the requester
     // that the operation is complete, before performing internal scheduler work (such as
     // starting the next client).
@@ -283,12 +282,15 @@ public class BiometricScheduler<T, U> {
      * @param gestureAvailabilityDispatcher may be null if the sensor does not support gestures
      *                                      (such as fingerprint swipe).
      */
-    public BiometricScheduler(@SensorType int sensorType,
+    public BiometricScheduler(Context context,@SensorType int sensorType,
             @Nullable GestureAvailabilityDispatcher gestureAvailabilityDispatcher) {
         this(new Handler(Looper.getMainLooper()), sensorType, gestureAvailabilityDispatcher,
                 IBiometricService.Stub.asInterface(
                         ServiceManager.getService(Context.BIOMETRIC_SERVICE)),
                 LOG_NUM_RECENT_OPERATIONS);
+
+        mCancel = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_fpCancelIfNotIdle);
     }
 
     /**
@@ -319,7 +321,7 @@ public class BiometricScheduler<T, U> {
 
     protected void startNextOperation() {
         if (mCurrentOperation != null) {
-            if (mCancelIfNotIdle && !mCurrentOperation.isFinished()) {
+            if (mCancel && !mCurrentOperation.isFinished()) {
                 Slog.v(TAG, "Not idle, cancelling current operation: " + mCurrentOperation);
                 mCurrentOperation.cancel(mHandler, mInternalCallback);
             } else {
