5dd0f13292e0f1311067129a456c7bbf83844ed4
clarencelol <clarencekuiek@proton.me>
2024-07-05 00:38:02 +0800

    Revert "Add an option to force pre-O apps to use full screen aspect ratio"
    This reverts commit 0198ea531c47f760406b3d7ecd717333ecf54d1e.

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -290,8 +290,6 @@ import com.android.server.uri.UriGrantsManagerInternal;
 import com.android.server.wallpaper.WallpaperManagerInternal;
 import com.android.wm.shell.Flags;
 
-import org.lineageos.internal.applications.LineageActivityManager;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileDescriptor;
@@ -806,9 +804,6 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     private Set<Integer> mProfileOwnerUids = new ArraySet<Integer>();
 
-    // Lineage sdk activity related helper
-    private LineageActivityManager mLineageActivityManager;
-
     private final class SettingObserver extends ContentObserver {
         private final Uri mFontScaleUri = Settings.System.getUriFor(FONT_SCALE);
         private final Uri mHideErrorDialogsUri = Settings.Global.getUriFor(HIDE_ERROR_DIALOGS);
@@ -908,10 +903,6 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     public void installSystemProviders() {
         mSettingsObserver = new SettingObserver();
-
-        // LineageActivityManager depends on settings so we can initialize only
-        // after providers are available.
-        mLineageActivityManager = new LineageActivityManager(mContext);
     }
 
     public void retrieveSettings(ContentResolver resolver) {
@@ -7389,9 +7380,5 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     static boolean isPip2ExperimentEnabled() {
         return Flags.enablePip2Implementation();
-    }
-
-    public boolean shouldForceLongScreen(String packageName) {
-        return mLineageActivityManager.shouldForceLongScreen(packageName);
     }
 }
